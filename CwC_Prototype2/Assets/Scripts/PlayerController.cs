e6eae2fcd67e5f934d43ee1d4e78f57b2829275b
AndreyGV <grechko.games@gmail.com>
2022-06-07 12:17:45 +0300

    2

--- CwC_Prototype2/Assets/Scripts/PlayerController.cs
+++ CwC_Prototype2/Assets/Scripts/PlayerController.cs
@@ -34,7 +34,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // launch a projectile from the player
-            Instantiate(projectilePrefab, transform.position, transform.rotation);
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
