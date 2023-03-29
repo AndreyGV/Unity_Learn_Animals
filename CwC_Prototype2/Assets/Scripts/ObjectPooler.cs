5d3152f83a7dc0b9e471227c579bf985d60f9463
AndreyGV <74981070+AndreyGV@users.noreply.github.com>
2023-03-29 23:22:02 +0300

    Update ObjectPooler.cs

--- CwC_Prototype2/Assets/Scripts/ObjectPooler.cs
+++ CwC_Prototype2/Assets/Scripts/ObjectPooler.cs
@@ -14,32 +14,27 @@ public class ObjectPooler : MonoBehaviour
         SharedInstance = this;
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Loop through list of pooled objects,deactivating them and adding them to the list 
         pooledObjects = new List<GameObject>();
         for (int i = 0; i < amountToPool; i++)
         {
             GameObject obj = (GameObject)Instantiate(objectToPool);
             obj.SetActive(false);
             pooledObjects.Add(obj);
-            obj.transform.SetParent(this.transform); // set as children of Spawn Manager
+            obj.transform.SetParent(this.transform);
         }
     }
 
     public GameObject GetPooledObject()
     {
-        // For as many objects as are in the pooledObjects list
         for (int i = 0; i < pooledObjects.Count; i++)
         {
-            // if the pooled objects is NOT active, return that object 
             if (!pooledObjects[i].activeInHierarchy)
             {
                 return pooledObjects[i];
             }
         }
-        // otherwise, return null   
         return null;
     }
 
